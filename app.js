2cb8ee49e2281f09a97cb601d8447b9231d7c640
vosw1 <vosw1@naver.com>
2024-08-30 19:56:19 +0900

    배열 이용하기

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-const amISmart = null;  // text가 아님
-let something; // 값이 선언되지 않음
+// 배열 만들기
+let daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-console.log(something);
+// 배열 전체 조회하기
+console.log(daysOfWeek);  
+
+// 배열 특정값 가져오기
+console.log(daysOfWeek[5]); 
+
+// 배열 추가하기
+daysOfWeek.push("holiday");
+console.log(daysOfWeek); 
+
+// 배열 특정값 삭제하기
+daysOfWeek = daysOfWeek.filter(day => day !== "holiday");
+console.log(daysOfWeek);  
+
+// map을 이용해서 배열의 특정값 변경하기
+daysOfWeek = daysOfWeek.map(day => day === "sun" ? "off" : day);
+console.log(daysOfWeek);  
+
+// index를 이용해서 배열의 특정값 변경하기
+daysOfWeek[6] = "vacation";
+console.log(daysOfWeek);  
