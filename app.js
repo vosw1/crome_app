cbd567147b0b42ddb536905b1a1d84f401ee65d1
vosw1 <vosw1@naver.com>
2024-08-30 23:30:44 +0900

    가정문 사용하기

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const player = {
-    name: "min",
-    sayHello: function(otherName) {
-        console.log("hello! " + otherName);
-    }
-};
+const age = parseInt(prompt("How old are you?"));
 
-// sayHello 메서드 호출 시, 문자열 인자를 전달해야 합니다.
-player.sayHello("song");
+if (isNaN(age)) { // 입력받은 값이 숫자가 아닐 때
+    console.log("숫자를 입력해주세요~");
+} else if (age < 18) { // 18세보다 어릴 때
+    console.log("18세 보다 어리시네요ㅜ");
+} else if (age >= 18 && age <= 50) { // 18세 이상이고 50세 이하일 때
+    console.log("원하는 음료를 마실 수 있으세요~!");
+} else { // 50세 초과일 때
+    console.log("원하는 음료를 마실 수 없으세요ㅠ");
+}
