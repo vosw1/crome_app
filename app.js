4879084d6f2003355d905ed078fc39857200b2c8
vosw1 <vosw1@naver.com>
2024-08-30 19:11:23 +0900

    const와 let의 차이점 확인하기

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const a = 10;
-const b = 2;
+let a = 10;
+let b = 2;
+
+/*
+const : 변할 수 없는 값
+let : 변할 수 있는 값
+변수명에는 공백이 있을 수 없음 -> 카멜 표기법 사용 */
+
+let myName = "min"; 
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
+console.log("hello " + myName);
+
+myName = "minkyung";
+
+console.log("my name is " + myName);
