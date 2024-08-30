088d287052f808fc5b7aed32129c6a987e527a6e
vosw1 <vosw1@naver.com>
2024-08-30 19:27:14 +0900

    다른 변수 더 알아보기

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-let a = 10;
-let b = 2;
+const amISmart = null;  // text가 아님
+let something; // 값이 선언되지 않음
 
-/*
-const : 변할 수 없는 값
-let : 변할 수 있는 값
-변수명에는 공백이 있을 수 없음 -> 카멜 표기법 사용 */
-
-let myName = "min"; 
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName);
-
-myName = "minkyung";
-
-console.log("my name is " + myName);
+console.log(something);
