1f64682fc4ce9aa8fc1e78bcfe46e1b7ed0f0b25
vosw1 <vosw1@naver.com>
2024-09-01 21:31:57 +0900

    js에서 css 적용하기

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-const h1 = document.querySelector(".hello h1:first-child"); // HTML의 요소 가져오기
+const h1 = document.querySelector("div.hello:first-child h1"); // HTML의 요소 가져오기
 
-console.dir(h1);
-
-function handleTilteClick(){ // 클릭시 이벤트
-    h1.style.color = "blue";
-    console.log("title을 클릭했어요!")
+function handleTitleClick() { // 클릭 시 이벤트
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") { 
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){ 
-    h1.innerText = "마우스는 여기 있어요!";
-}
-
-function handleMouseLeave(){ 
-    h1.innerText = "마우스는 여기 없어요!";
-}
-
-function handleWindowResize(){ 
-    document.body.style.backgroundColor = "tomato";
-}
-
-
-h1.addEventListener("click", handleTilteClick); // 조건 : 클릭, 일어나는 이벤트
-h1.addEventListener("mouseenter", handleMouseEnter); 
-h1.addEventListener("mouseleave", handleMouseLeave); 
-
-
-window.addEventListener("resize", handleWindowResize);
+h1.addEventListener("click", handleTitleClick); // 클릭 시 handleTitleClick 함수 호출
