2b030e779e5b59bfb82df7fe5b62e3b980c3d5e8
vosw1 <vosw1@naver.com>
2024-09-02 12:41:38 +0900

    a태그의 기본동작(링크 이동) 막기

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInut = document.querySelector("#login-form input"); // input 태그 가져오기
 
+const link = document.querySelector("a"); // a태그 가져오기
+
 function onLoginSubmit(event) { // 자리만 만들어주면 됨
     event.preventDefault(); // form 태그 submit의 기본 동작(새로고침) 막기
     console.log(loginInut.value);
 }
 
+function handleLinkClick(event) { 
+    event.preventDefault(); 
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
