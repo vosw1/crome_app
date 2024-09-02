684c84f3c62a1ff34948fbae239c17f949b6cfca
vosw1 <vosw1@naver.com>
2024-09-02 12:16:15 +0900

    form 태그 submit의 기본 동작(새로고침) 방지

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInut = document.querySelector("#login-form input"); // input 태그 가져오기
-const loginButton = document.querySelector("#login-form button"); // 버튼 가져오기
 
-function handleBtnClick() {
-    const username = loginInut.value;
-    console.log(username);
+function onLoginSubmit(event) { // 자리만 만들어주면 됨
+    event.preventDefault(); // form 태그 submit의 기본 동작(새로고침) 막기
+    console.log(loginInut.value);
 }
 
-loginButton.addEventListener("click", handleBtnClick);
+loginForm.addEventListener("submit",onLoginSubmit);
