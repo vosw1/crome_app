6f27022a3aaff7b12f13b1bfdb6694b41fcd467d
vosw1 <vosw1@naver.com>
2024-09-02 20:50:18 +0900

    todo 리스트 삭제하기

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newToDo) {
     const li = document.createElement("li"); // li 요소 만들기
      const span = document.createElement("span"); // span 요소 만들기
-     li.appendChild(span); // span 요소에 li 추가하기
      span.innerText = newToDo; // li 내부에 텍스트 설정하기
+     const button = document.createElement("button") // button 요소 만들기
+     button.innerText = "❌";
+     button.addEventListener("click", deleteToDo);
+     li.appendChild(span); // span 요소에 li 추가하기
+     li.appendChild(button); //  span 요소에 button 추가하기
      toDoList.appendChild(li);
 }
 
