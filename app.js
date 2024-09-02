f349ee25226dea09ce1e98fc0db016068f20a41d
vosw1 <vosw1@naver.com>
2024-09-02 10:14:57 +0900

    input과 button 이용하기

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const h1 = document.querySelector("div.hello:first-child h1"); // HTML의 요소 가져오기
+const loginInut = document.querySelector("#login-form input"); // input 태그 가져오기
+const loginButton = document.querySelector("#login-form button"); // 버튼 가져오기
 
-function handleTitleClick() {
-    h1.classList.toggle("clicked");
+function handleBtnClick() {
+    console.log(loginInut.value);
+    console.log("클릭했어요");
 }
+
+loginButton.addEventListener("click", handleBtnClick);
