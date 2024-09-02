dc60877fdf68075f626b8ca02b900c9018fa1345
vosw1 <vosw1@naver.com>
2024-09-02 13:38:49 +0900

    입력받은 값을 local storage에 저장하기

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const loginForm = document.querySelector("#login-form");
-const loginInut = document.querySelector("#login-form input"); // input 태그 가져오기
+const loginInput = document.querySelector("#login-form input");
+const freeting =- document.querySelector("#greeting");
 
-const link = document.querySelector("a"); // a태그 가져오기
+const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginSubmit(event) { // 자리만 만들어주면 됨
-    event.preventDefault(); // form 태그 submit의 기본 동작(새로고침) 막기
-    console.log(loginInut.value);
-}
-
-function handleLinkClick(event) { 
-    event.preventDefault(); 
-    console.dir(event);
+function onLoginSubmit(event) { 
+    event.preventDefault(); // 새로고침 방지
+    loginForm.classList.add(HIDDEN_CLASSNAME); // 폼 숨기기
+    const username = loginInput.value; // loginInput에 값을 변수에 저장하기
+    localStorage.setItem("username", username); // 키와 값의 쌍으로 저장하기
+    greeting.innerText = `Hello ${username}`; //저장된 값을 변수 값에 넣기
+    greeting.classList.remove(HIDDEN_CLASSNAME); // hidden 클래스명 없애기
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
