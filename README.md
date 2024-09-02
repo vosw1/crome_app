e29056a1c7bb44e69d8b142476e0297962f97fc8
vosw1 <vosw1@naver.com>
2024-09-02 22:59:53 +0900

    storage에 저장된 내용 목록으로 가져오기 및 삭제하기

--- README.md
+++ README.md
@@ -163,3 +163,69 @@ console.log(timestamp);
 - img 요소 추가하기 : `const bgImage = document.createElement("img"); `
 - 이미지 경로 설정하기 : `bgImage.src = `img/${choseImage}`;`
 - body 내부에 추가하기 : `document.body.appendChild(bgImage);`
+
+### todo List
+
+- 입력하기
+
+```const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+// = const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function handleToDoSubmit(event) {
+    event.preventDefault(); // 새로고침 막기
+    console.log(toDoInput.value);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+```
+
+- 저장하기
+
+```function paintToDo(newToDo) {
+    const li = document.createElement("li"); // li 요소 만들기
+    const span = document.createElement("span"); // span 요소 만들기
+    span.innerText = newToDo; // span 내부에 텍스트 설정하기
+    li.appendChild(span); // span을 li에 추가하기
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault(); // 새로고침 막기
+    const newToDo = toDoInput.value; // 입력 필드의 값 저장
+    toDoInput.value = ""; // 입력 필드 비우기
+    toDos.push(newToDo); // 배열에 추가하기
+    paintToDo(newToDo); // 저장된 값을 paintToDo 함수로 전달
+    saveToDos(); // localStorage에 저장하기
+}
+```
+
+- 삭제하기
+
+```function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newToDo) {
+   ...
+    const button = document.createElement("button"); // button 요소 만들기
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span); // span을 li에 추가하기
+    li.appendChild(button); // button을 li에 추가하기
+    toDoList.appendChild(li);
+}
+```
+
+- 목록보기
+
+```const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos; // 이전 값 복원 해주기
+    parsedToDos.forEach(paintToDo);
+}
+```
--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+
+let toDos = []; // 비어있는 배열의 상태로 시작
 
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
@@ -15,14 +17,14 @@ function deleteToDo(event) {
 
 function paintToDo(newToDo) {
     const li = document.createElement("li"); // li 요소 만들기
-     const span = document.createElement("span"); // span 요소 만들기
-     span.innerText = newToDo; // li 내부에 텍스트 설정하기
-     const button = document.createElement("button") // button 요소 만들기
-     button.innerText = "❌";
-     button.addEventListener("click", deleteToDo);
-     li.appendChild(span); // span 요소에 li 추가하기
-     li.appendChild(button); //  span 요소에 button 추가하기
-     toDoList.appendChild(li);
+    const span = document.createElement("span"); // span 요소 만들기
+    span.innerText = newToDo; // span 내부에 텍스트 설정하기
+    const button = document.createElement("button"); // button 요소 만들기
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span); // span을 li에 추가하기
+    li.appendChild(button); // button을 li에 추가하기
+    toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
@@ -31,7 +33,16 @@ function handleToDoSubmit(event) {
     toDoInput.value = ""; // 입력 필드 비우기
     toDos.push(newToDo); // 배열에 추가하기
     paintToDo(newToDo); // 저장된 값을 paintToDo 함수로 전달
-    saveToDos(); // local Storage에 저장하기
+    saveToDos(); // localStorage에 저장하기
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// 저장된 todos 불러오기
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos; // 이전 값 복원 해주기
+    parsedToDos.forEach(paintToDo); 
+} 
