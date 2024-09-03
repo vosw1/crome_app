ba672566288998a3a492d539c03fd98bfaf7bfcb
vosw1 <vosw1@naver.com>
2024-09-03 22:38:33 +0900

    qoute css 적용하기

--- js/quotes.js
+++ js/quotes.js
@@ -1,50 +1,50 @@
 const quotes = [
     {
-        quote: "The only way to do great work is to love what you do.",
+        quote: "The only way to do great work is to love what you do",
         author: "Steve Jobs"
     },
     {
-        quote: "Life is what happens when you're busy making other plans.",
+        quote: "Life is what happens when you're busy making other plans",
         author: "John Lennon"
     },
     {
-        quote: "The purpose of our lives is to be happy.",
+        quote: "The purpose of our lives is to be happy",
         author: "Dalai Lama"
     },
     {
-        quote: "Get busy living or get busy dying.",
+        quote: "Get busy living or get busy dying",
         author: "Stephen King"
     },
     {
-        quote: "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+        quote: "You have within you right now, everything you need to deal with whatever the world can throw at you",
         author: "Brian Tracy"
     },
     {
-        quote: "Believe you can and you're halfway there.",
+        quote: "Believe you can and you're halfway there",
         author: "Theodore Roosevelt"
     },
     {
-        quote: "The best way to predict the future is to invent it.",
+        quote: "The best way to predict the future is to invent it",
         author: "Alan Kay"
     },
     {
-        quote: "Your time is limited, don't waste it living someone else's life.",
+        quote: "Your time is limited, don't waste it living someone else's life",
         author: "Steve Jobs"
     },
     {
-        quote: "If life were predictable it would cease to be life, and be without flavor.",
+        quote: "If life were predictable it would cease to be life, and be without flavor",
         author: "Eleanor Roosevelt"
     },
     {
-        quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
-        author: "Martin Luther King Jr."
+        quote: "In the end, we will remember not the words of our enemies, but the silence of our friends",
+        author: "Martin Luther King Jr"
     }
 ];
 
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+const quote = document.querySelector("#quote div:first-child");
+const author = document.querySelector("#quote div:last-child");
 
 const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todayQuote.quote;
-author.innerText = todayQuote.author;
+author.innerText = `by ${todayQuote.author}`;
