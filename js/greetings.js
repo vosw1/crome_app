f2f088ccde9f2eb468ef0a7f4993045cf9de9e90
vosw1 <vosw1@naver.com>
2024-09-03 22:23:03 +0900

    login-form css 적용하기

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,6 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const freeting =- document.querySelector("#greeting");
+const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -14,14 +14,14 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`; //저장된 값을 변수 값에 넣기
+    greeting.innerText = `Hello ${username}`; // 저장된 값을 변수 값에 넣기
     greeting.classList.remove(HIDDEN_CLASSNAME); // hidden 클래스명 없애기
 }
 
-const saveUsername = localStorage.getItem(USERNAME_KEY);
-if(saveUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit",onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME); // 로그인 폼 보이기
+    loginForm.addEventListener("submit", onLoginSubmit); // 로그인 폼에 이벤트 리스너 추가
 } else {
-    paintGreetings(saveUsername);
+    paintGreetings(savedUsername); // 저장된 사용자명으로 인사말 표시
 }
