ec7f08558db9aea323e3210acf782f20643fedfa
vosw1 <vosw1@naver.com>
2024-09-03 21:02:38 +0900

    현재 위치의 날씨 추가하기

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,20 @@
+const API_KEY = "46c42ba74d2d22c044c652e13464ef9b";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(`당신은 ${lat} ${lng} 있어요~`);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => 
+        response.json()).then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError(){
-    alert("위치를 찾을 수 없어요. 날씨를 찾을 수 없어요ㅜ")
+    alert("위치를 찾을 수 없어요. 날씨를 찾을 수 없어요ㅜ");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
