bf133ed45bb129b9a1ae207554093aa7401233c4
vosw1 <vosw1@naver.com>
2024-09-03 21:47:30 +0900

    배경 이미지 id 추가 후 css 적용하기

--- js/background.js
+++ js/background.js
@@ -1,9 +1,11 @@
-const images = ["desk.jpeg", "sky.jpeg", "simple.png"];
+const images = ["sky.jpeg", "sky2.jpg"];
 
 const choseImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img"); // img 추가
 
 bgImage.src = `img/${choseImage}`; // 이미지 경로 설정
+
+bgImage.id = "bg-img"; // id 추가
 
 document.body.appendChild(bgImage); // body 내부에 추가하기
